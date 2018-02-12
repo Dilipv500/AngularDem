1bea6de3045d8f0a31c3b3510e0d14e619a95b22
Dilip <dilip.v500@gmail.com>
2018-02-12 12:29:09 +0530

    format fix

--- src/app/dilip/dilip.component.ts
+++ src/app/dilip/dilip.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dilip',
-  //selector:".app-dilip",
+  // selector:".app-dilip",
   templateUrl: './dilip.component.html',
 //  template:`dilip it works`,
   styleUrls: ['./dilip.component.css']
 })
 export class DilipComponent {
-  serverId:number=1;
-serverstatus:string="Active";
-getserverstatus(){
-  return "Server Id "+this.serverId + "Server Status  " +this.serverstatus;
-};
+  serverId = 1;
+serverstatus = 'Active';
+getserverstatus() {
+  return 'Server Id ' + this.serverId + 'Server Status  ' + this.serverstatus;
+}
 }
